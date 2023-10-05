e4b1d1a4de9325efb016232acf9c631ff3862c12
Fabianojs <119818261+Litrixjs@users.noreply.github.com>
2023-10-05 17:05:27 -0300

    Update desafio.js
    refiz o desafio colocando a sinal de igual antes do maior e menor.
    para incluir a numero dentro da sentença.

--- desafio.js
+++ desafio.js
@@ -1,20 +1,20 @@
 let nomeHeroi = "Bastião"
-let XP = 10500
+let XP = 10001
 let nivelHeroico = 0
 
 if (XP < 1000) {
     nivelHeroico = "Ferro"
-} else if (XP > 1001 && XP < 2000) {
+} else if (XP >= 1001 && XP <= 2000) {
     nivelHeroico = "Bronze"
-} else if (XP > 2001 && XP < 5000) {
+} else if (XP >= 2001 && XP <= 5000) {
     nivelHeroico = "Prata"
-} else if (XP > 5001 && XP < 7000) {
+} else if (XP >= 5001 && XP <= 7000) {
     nivelHeroico = "Ouro"
-} else if (XP > 7001 && XP < 8000) {
+} else if (XP >= 7001 && XP <= 8000) {
     nivelHeroico = "Platina"
-} else if (XP > 8001 && XP < 9000) {
+} else if (XP >= 8001 && XP <= 9000) {
     nivelHeroico = "Ascendente"
-} else if (XP > 9001 && XP < 10000) {
+} else if (XP >= 9001 && XP <= 10000) {
     nivelHeroico = "Imortal"
 } else {
     nivelHeroico = "Radiante"
